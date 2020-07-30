4c2b7756a237b258d6ee6561c4d483883c6273d3
Andy Leverenz <andy@justalever.com>
2020-07-30 14:55:14 -0500

    Update user model with developer and employer personas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_193612) do
+ActiveRecord::Schema.define(version: 2020_07_30_194438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,8 @@ ActiveRecord::Schema.define(version: 2020_07_30_193612) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "moderator"
+    t.boolean "developer", default: false
+    t.boolean "employer", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
