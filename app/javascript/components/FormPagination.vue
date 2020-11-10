d8f7876eb97f291b4d8d371f167fd8d7fd4b409e
Andy Leverenz <andy@justalever.com>
2020-11-10 13:14:07 -0600

    Adds preview step to new job form. Tailwind config reformat

---
+++ app/javascript/components/FormPagination.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="fixed bottom-0 left-0 right-0 flex items-center w-full py-3 bg-black border-t bg-opacity-95 lg:px-8"
+    :class="position"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    position: {
+      required: true,
+      type: String,
+    },
+  },
+};
+</script>
---
+++ app/javascript/components/preview/PreviewContent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="lg:col-span-8">
+    <div class="lg:pr-6">
+      <h1 class="text-4xl font-black">{{ $store.form.job.title }}</h1>
+      <p class="mt-4 text-xs font-black tracking-wider text-gray-600 uppercase">
+        Description
+      </p>
+      <div v-html="$store.form.job.description" class="mb-6"></div>
+
+      <div v-if="$store.form.job.companyDescription">
+        <p
+          class="mt-4 text-xs font-black tracking-wider text-gray-600 uppercase"
+        >
+          Company Description
+        </p>
+
+        <div v-html="$store.form.job.companyDescription"></div>
+      </div>
+    </div>
+    <a
+      :href="$store.form.job.linkToApply"
+      target="_blank"
+      class="my-6 text-center btn btn-red btn-lg"
+      >Apply for this role</a
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
